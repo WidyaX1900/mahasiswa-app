77282f8f50f4fdef0a9c628fe464c125375a3e88
WidyaX1900 <tuderangga93@gmail,com>
2025-07-20 14:30:29 +0800

    view: setup layout view

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'assets_url' => env('ASSETS_URL', 'http://localhost/mahasiswa-app/resources/'),
 
     /*
     |--------------------------------------------------------------------------
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mahasiswa App</title>
+    <link rel="stylesheet" href="{{ config('app.assets_url') }}bootstrap/css/bootstrap.min.css">
+    <script src="{{ config('app.assets_url') }}js/jquery-3.7.1.js"></script>
+</head>
+<body>
+    @yield('navbar')
+    @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"
+        integrity="sha384-7qAoOXltbVP82dhxHAUje59V5r2YsVfBafyUDxEdApLPmcdhBPg1DKg1ERo0BZlK" crossorigin="anonymous">
+    </script>
+</body>
+</html>
